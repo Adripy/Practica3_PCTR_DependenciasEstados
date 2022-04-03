da56038ea70917b33fc6785071db48fa3009c9af
Adripy <36918572+Adripy@users.noreply.github.com>
2022-04-03 18:26:42 +0200

    IParque agregados metodos necesarios

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,7 @@ package src.p03.c01;
 
 public interface IParque {
 	
-	public abstract void entrarAlParque(String puerta);
-
-	// TODO
+	public abstract void entrarAlParque(String puerta); // metodo entar al parque que recibe una puerta
+	public abstract void salirDelParque(String puerta); // metodo salir al parque que recibe una puerta
 
 }
